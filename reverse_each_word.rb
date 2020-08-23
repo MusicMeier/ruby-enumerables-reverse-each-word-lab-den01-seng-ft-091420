38908ad52904096c38ae6bdb54c50854f3c0ac57
Music Meier <musicm987@gmail.com>
2020-08-23 01:35:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,11 +2,18 @@ def reverse_each_word(string)
   new_array = []
   new_array << string.split
 
-result = new_array.collect do |words|
+  result = new_array.collect do |words|
    words.reverse
-  
   end
 result.join
 end
   
-  
+  def reverse_each_word(string)
+  new_array = []
+  new_array << string.split
+
+  result = new_array.collect do |words|
+   words.reverse
+  end
+result.join
+end
